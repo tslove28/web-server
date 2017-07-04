90610ada4e348120d2826c670e4111bc5c0ae4a8
Tom Simpson <tom@tomandkristi.com>
2017-07-04 16:03:35 -0700

    update port for herok

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js');
-// var middleware = {
-//     requireAuthentication: function (req, res, next) {
-//         console.log('Private route hit!');
-//         next();
-//     }, 
-//     logger: function (req, res, next){
-//         console.log('request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl );
-//         next();
-//     }
-// };
 
 app.use(middleware.logger);
 
@@ -24,4 +14,4 @@ app.use(express.static(__dirname +'/public'));
 
 app.listen(PORT, function () {
     console.log('Express server started' + " on port " + PORT + '!!');
-});
+}); 
